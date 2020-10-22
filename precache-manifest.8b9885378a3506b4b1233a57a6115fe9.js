e0c78333247f2e2768b4d282e691c4181afc8d43
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2020-10-21 23:45:45 -0400

    Updates

---
+++ precache-manifest.8b9885378a3506b4b1233a57a6115fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b1c70a8e10ab9e57955f46e38d8ffe",
+    "url": "/naruto-watcher/index.html"
+  },
+  {
+    "revision": "b5dd4effd3dd9dd8ecce",
+    "url": "/naruto-watcher/static/css/main.bee24e70.chunk.css"
+  },
+  {
+    "revision": "9418cd3eaaa4733a6ac6",
+    "url": "/naruto-watcher/static/js/2.0a6ed0d3.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/naruto-watcher/static/js/2.0a6ed0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dd4effd3dd9dd8ecce",
+    "url": "/naruto-watcher/static/js/main.f131fb6c.chunk.js"
+  },
+  {
+    "revision": "aa6c6f28647416951031",
+    "url": "/naruto-watcher/static/js/runtime-main.238079ed.js"
+  }
+]);
