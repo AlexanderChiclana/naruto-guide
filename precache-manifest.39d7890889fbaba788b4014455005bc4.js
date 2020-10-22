29a70007ef4feef7975da747f6afbd2520ca4445
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2020-10-21 23:49:38 -0400

    Updates

---
+++ precache-manifest.39d7890889fbaba788b4014455005bc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99eb364d64e60e30f0ea1e63cf95f73",
+    "url": "/naruto-guide/index.html"
+  },
+  {
+    "revision": "eaa6d156d5f74532c51e",
+    "url": "/naruto-guide/static/css/main.bee24e70.chunk.css"
+  },
+  {
+    "revision": "9418cd3eaaa4733a6ac6",
+    "url": "/naruto-guide/static/js/2.0a6ed0d3.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/naruto-guide/static/js/2.0a6ed0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa6d156d5f74532c51e",
+    "url": "/naruto-guide/static/js/main.772b151b.chunk.js"
+  },
+  {
+    "revision": "7c3fa5ce59b65305f8da",
+    "url": "/naruto-guide/static/js/runtime-main.2a80414a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/naruto-watcher/precache-manifest.8b9885378a3506b4b1233a57a6115fe9.js"
+  "/naruto-guide/precache-manifest.39d7890889fbaba788b4014455005bc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/naruto-watcher/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/naruto-guide/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
